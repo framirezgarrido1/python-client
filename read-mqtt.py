c59b7072bdce5b68970ad1e205ad86c66333661d
FerPI <fernando.rg@live.cl>
2020-03-12 14:47:38 -0300

    add python file

---
+++ read-mqtt.py
@@ -0,0 +1,24 @@
+import paho.mqtt.client as mqtt
+
+# This is the Subscriber
+
+def on_connect(client, userdata, flags, rc):
+  print("Connected with result code "+str(rc))
+  client.subscribe("devices/esp8266/#")
+
+def on_message(client, userdata, msg):
+
+	message = msg.payload.decode()
+	print("")
+	x = message.split("-")
+	print("id > "+ x[0] + " state > " + x[1])
+
+   	#client.disconnect()
+
+client = mqtt.Client()
+client.connect("broker.hivemq.com",1883,60)
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.loop_forever()
