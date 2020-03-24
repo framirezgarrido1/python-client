c919b51efbbe1008e01ef68809398b5f2907638b
framirezgarrido1 <f.ramirez@globant.com>
2020-03-24 15:52:45 -0300

    update task

--- rain-days.py
+++ rain-days.py
@@ -1,11 +1,12 @@
+# Schedule Library imported
+
 import requests
 import schedule 
-import time
+import time 
 
 
 response = requests.get('http://ec2-3-17-162-210.us-east-2.compute.amazonaws.com:3002/api/devices') 
 print response.content
-
 
 def rainactive(): 
 	response = requests.put('http://ec2-3-17-162-210.us-east-2.compute.amazonaws.com:3002/api/update/5e5574b3421a35568fe20657/0')
@@ -29,3 +30,10 @@ schedule.every().thursday.at("22:12").do(raininactive)
 #Sábado
 schedule.every().saturday.at("22:10").do(rainactive)
 schedule.every().saturday.at("22:12").do(raininactive) 
+
+while True: 
+
+	# Checks whether a scheduled task 
+	# is pending to run or not 
+	schedule.run_pending() 
+	time.sleep(1) 
