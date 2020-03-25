faca99c612e53adf8132f2c4cca21dcae9f9a754
framirezgarrido1 <f.ramirez@globant.com>
2020-03-24 22:45:38 -0300

    update task

--- rain-days.py
+++ rain-days.py
@@ -9,17 +9,17 @@ response = requests.get('http://ec2-3-17-162-210.us-east-2.compute.amazonaws.com
 print response.content
 
 def rainactive(): 
-    response = requests.put('http://ec2-3-17-162-210.us-east-2.compute.amazonaws.com:3002/api/update/5e5574b3421a35568fe20657/0')
+    response = requests.put('http://ec2-3-17-162-210.us-east-2.compute.amazonaws.com:3002/api/update/5e5574b3421a35568fe20658/0')
     print("Rain Active")
 
 def raininactive(): 
-    response = requests.put('http://ec2-3-17-162-210.us-east-2.compute.amazonaws.com:3002/api/update/5e5574b3421a35568fe20657/1')
+    response = requests.put('http://ec2-3-17-162-210.us-east-2.compute.amazonaws.com:3002/api/update/5e5574b3421a35568fe20658/1')
     print("Rain Inactive")
 
 
 #Martes
-schedule.every().tuesday.at("22:10").do(rainactive)
-schedule.every().tuesday.at("22:12").do(raininactive) 
+schedule.every().tuesday.at("22:50").do(rainactive)
+schedule.every().tuesday.at("22:52").do(raininactive) 
 
 #Jueves
 schedule.every().thursday.at("22:10").do(rainactive)
